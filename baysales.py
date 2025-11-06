521d48033c3a8c37af6255487ae620903462181c
Pedro Rafful <pedro.rafful2@gmail.com>
2025-11-06 09:00:12 -0300

    fixtentar

--- baysales.py
+++ baysales.py
@@ -14,8 +14,10 @@ BEYSALES - Gerente de Estoque
 
 def tentar_novamente():
     while True:
-        t = input("Deseja rodar o programa novamente? (S/N)")
+        t = input("Deseja rodar o programa novamente? (S/N) ")
         if t.lower() == "s":
+            os.system("cls")
+            print(tit)
             return True
         elif t.lower() == "n":
          print("Fim do programa")
@@ -30,8 +32,10 @@ def cadastrar_beyblade():
         while True:
             codigo = input("Código do Beyblade: ")
             if codigo in [line.split(",")[0] for line in open("beysales.txt", "r", encoding="utf-8").readlines()]:
+             os.system("cls")
              print("Código já existe. Tente novamente ou atualize o beyblade.")
             elif codigo.isdigit() == False:
+             os.system("cls")
              print("Código deve ser um número inteiro. Tente novamente.")
             else:
              break
@@ -39,60 +43,77 @@ def cadastrar_beyblade():
         while True:
             tipo = input("Tipo (Ataque, Defesa, Equilíbrio): ")
             if tipo not in [line.split(",")[1] for line in open("beysales.txt", "r", encoding="utf-8").readlines()]:
+             os.system("cls")
              print("Tipo inválido. Tente novamente.")
             else:
+             os.system("cls")
              break
          
         while True: 
             nome = input("Nome do Beyblade: ")
             if nome.strip() == "":
+             os.system("cls")
              print("Nome não pode ser vazio. Tente novamente.")
             elif nome in [line.split(",")[2] for line in open("beysales.txt", "r", encoding="utf-8").readlines()]:
+             os.system("cls")
              print("Nome já existe. Tente novamente ou atualize o beyblade.")
             elif not all(ch.isalpha() or ch.isspace() for ch in nome):
+             os.system("cls")
              print("Nome deve conter apenas letras. Tente novamente.") 
             else:
+             os.system("cls")
              break
 
         while True:
             try:
-                preco = float(input("Preço: "))
+                preco = float(input("Preço: ").replace(",","."))
                 if preco < 0:
+                    os.system("cls")
                     print("Preço deve ser um número positivo. Tente novamente.")
             except ValueError:
+                os.system("cls")
                 print("Preço deve ser um número válido. Tente novamente.")
             else:
+               os.system("cls")
                break
             
         while True:
             try:
                 quantidade = int(input("Quantidade em estoque: "))
                 if quantidade < 0:
+                    os.system("cls")
                     print("Quantidade deve ser um número positivo. Tente novamente.")
             except ValueError:
+                os.system("cls")
                 print("Quantidade deve ser um número inteiro válido. Tente novamente.")
             else:
+               os.system("cls")
                break
         
         f.write(f"{codigo},{tipo},{nome},{preco},{quantidade}\n")
+    os.system("cls")
     print("Beyblade cadastrado com sucesso!")
 
 def main():
     print(tit)
     while True:
         try:
-         esc = int(input("Qual operação deseja realizar? (0-5)"))
+         esc = int(input("Qual operação deseja realizar? (0-5) "))
          if esc < 0 or esc > 5:
             print("Escolha uma opção entre 0 e 5.")
         except ValueError:
+            os.system("cls")
             print("Insira um valor válido")
         else:
+            os.system("cls")
             if esc == 1:
                 cadastrar_beyblade()
                 if not tentar_novamente():
+                   os.system("cls")
                    break
 
             elif esc == 0:
+               os.system("cls")
                print("Fim do programa!")
                break
             
